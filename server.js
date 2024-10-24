7cd68650757ba9e60e2593aff15d6d763d38ff7a
sushil12code <sushilmahara5050@gmail.com>
2024-10-24 20:17:21 +0545

    Update server.js

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
