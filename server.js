f5dd5b69e67d9456ab5909f63486bde95e81369e
sushil12code <sushilmahara5050@gmail.com>
2024-10-24 20:11:46 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected: ' + socket.id);
+
+    // Listen for chat messages
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);  // Broadcast message to all users
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected: ' + socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
